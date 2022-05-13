ee331cfb1099dfb591118e52d4d0ec35029de4e8
Xanul <rivas.060@gmail.com>
2022-05-13 12:29:09 -0500

    Added the functionality of ask for a card button

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -10,6 +10,16 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 let especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+// Referencias HTML
+const btnPedir = document.querySelector('#btnPedir');
+const scoresJuego = document.querySelectorAll('small');
+const divJugadorCartas = document.querySelector('#jugador-cartas')
+
+
+
 const crearDeck = () => {
 
     for(i = 2; i <= 10; i++){
@@ -33,7 +43,6 @@ const pedirCarta= () => {
         throw 'No hay cartas en el deck'
     }
     let carta = deck.pop()
-    console.log(carta)
     return carta
     
 }
@@ -47,10 +56,34 @@ const valorCarta = (carta) => {
 }
 
 crearDeck();
+
 console.log(deck)
 
-const test = valorCarta(pedirCarta());
-console.log(test)
+
+// Eventos
+btnPedir.addEventListener('click', () => {
+    
+    let carta = pedirCarta();
+    
+    puntosJugador = puntosJugador + valorCarta(carta);
+    scoresJuego[0].innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `./assets/cartas/${carta}.png`
+    imgCarta.classList.add('carta')
+    divJugadorCartas.append(imgCarta);
+    
+    if( puntosJugador > 21 ) {
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+    } else if ( puntosJugador === 21 ) {
+        console.warn('BlackJack!')
+        btnPedir.disabled = true;
+    }
+
+   
+});
+
 
 
 
